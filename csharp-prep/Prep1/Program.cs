75647a5a1c6890c3dbcb0bd9c49ab77acb1436e8
sousaallan <allan2024252627@gmail.com>
2024-01-14 00:50:05 -0300

    Program one.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is you first name: ");
+        string f_name = Console.ReadLine();
+
+        Console.Write("What is you last name: ");
+        string l_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
     }
 }
