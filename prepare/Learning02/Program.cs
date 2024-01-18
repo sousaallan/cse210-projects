186c70fb5d99fac2e45546835c0de2f9f50166f4
sousaallan <allan2024252627@gmail.com>
2024-01-17 21:09:53 -0300

    Abstraction LA

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Doctor";
+        job1._company = "(Hospital Creek)";
+        job1._startYear = 1990;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Doctor cirgury";
+        job2._company = "(Hospital Marry)";
+        job2._startYear = 1980;
+        job2._endYear = 1990;
+
+        Resume Mresume = new Resume();
+        Mresume._name = "Allan Matos";
+
+        Mresume._jobs.Add(job1);
+        Mresume._jobs.Add(job2);
+
+        Mresume.Display();
     }
 }
